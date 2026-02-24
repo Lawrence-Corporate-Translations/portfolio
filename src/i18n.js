c2219ac2a193ee2bbbb01e769ebb82bd6c99bbe4
aharonml123-cloud <aharonml123@gmail.com>
2026-02-24 18:47:08 +0200

    Update i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -1,48 +1,16 @@
-// i18n.js
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import en from "./assets/i18n/en.json";
+import he from "./assets/i18n/he.json";
 
-import en from './assets/i18n/en.json';
-import he from './assets/i18n/he.json';
-
-const initI18n = async () => {
-  await i18n
-    .use(LanguageDetector)
-    .use(initReactI18next)
-    .init({
-      resources: {
-        en: { translation: en },
-        he: { translation: he },
-      },
-      fallbackLng: 'en',
-      debug: process.env.NODE_ENV === 'development',
-      
-      detection: {
-        order: ['localStorage', 'navigator', 'htmlTag'],
-        caches: ['localStorage'],
-      },
-      
-      interpolation: {
-        escapeValue: false,
-      },
-      
-      // Critical: Make sure React components re-render on language change
-      react: {
-        bindI18n: 'languageChanged',
-        bindI18nStore: '',
-        transEmptyNodeValue: '',
-        transSupportBasicHtmlNodes: true,
-        transKeepBasicHtmlNodesFor: ['br', 'strong', 'i'],
-        useSuspense: false,
-      },
-      
-      returnObjects: false,
-      returnEmptyString: false,
-      returnNull: false,
-    });
-};
-
-initI18n();
+i18n.use(initReactI18next).init({
+  resources: {
+    en: { translation: en },
+    he: { translation: he }
+  },
+  lng: "he", // Default to Hebrew
+  fallbackLng: "en",
+  interpolation: { escapeValue: false }
+});
 
 export default i18n;
