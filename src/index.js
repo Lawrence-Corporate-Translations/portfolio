f47ca1d3d4ccbf7b3eb0879f6dbd3200528d549a
aharonml123-cloud <aharonml123@gmail.com>
2026-02-24 18:42:47 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import "./i18n.js"
+import "./i18n.js";
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
     <App />
-  </BrowserRouter>
 );
